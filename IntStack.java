5af582863b6a7107eeacc47674eb26a0dbf16880
menhaj <menhaj007@yahoo.com>
2021-04-21 11:58:04 -0400

    minor changes

--- IntStack.java
+++ IntStack.java
@@ -4,64 +4,59 @@ public class IntStack {
 
     public IntStack(int defaultCapacity) {
         data = new int[defaultCapacity]; //instantiating the stack array.
-        topIndexLocation = -1; // Initially, the top-index position points outside of the array.
+        topIndexLocation = -1;
+        // Initially, the top-index position points outside of the array.
         // When the push method is called, the topIndexLocation gets incremented first such
         // as -1 + 1 =0, will give the first index then the new value will be added.
-        //topIndexLocation = 0; // This an alternative to setting to -1. If you chose
+        //topIndexLocation = 0; // This an alternative to setting index to -1. If you chose
         // to start from 0, then the value at data[0] will be inserted first,
-        // then increment the topIndexLocation for the next input.
+        // then the topIndexLocation get incremented.
     }
     public boolean isEmpty() {
-        return (topIndexLocation == -1); // This just weather topIndexLocation is true or false.
-        // Below is the alternative of way of writing
+        return (topIndexLocation == -1);
+        // This compares topIndexLocation against -1 and returns either true or false.
+        // Below is the alternative of way of writing line 16
          //if (topIndexLocation == -1) {
-             //return false;
+             //return true;
          //} else {
-           //  return true;
+           //  return false;
         //}
     }
     public boolean isFull() {
         return (topIndexLocation == data.length-1);
-        // Check if the number of elements equals to the size of the array.
-        //Since topIndexLocation is set to -1, it is important to subtract 1 from data.length because index
-        // numbers start at 0.
-        //It is also common to start with topIndexLocation = 0. In this case, compare topIndexLocation == data.length.
-        // if (topIndexLocation == data.length -1) { // topIndexLocation equals to the index of the last element
-        //  return true;
-        // } else {
-        //  return false;
-        // }
     }
 
     public void push(int value) {
         if (!isFull()) {
-            topIndexLocation++; // Notice, the topIndexLocation is -1, to adding -1 + 1 = 0.
+            topIndexLocation++;
+            // Notice, the topIndexLocation is -1. Adding -1 + 1 = 0.
             // Therefore topIndexLocation must become 0 for the first value to be inserted
-           // System.out.println(topIndexLocation + " at location " + value); // add print
-            // statement to see result
+           // System.out.println(topIndexLocation + " at location " + value);
+            // add print statement to see result
             data[topIndexLocation] = value;
         } else { // This is not necessary, but adding it will help you to see the error
             throw new IndexOutOfBoundsException("Stack is full");
         }
-        //if (!isFull()) data[++topIndexLocation] = value ;//if you prefer one line. Since this is
-        // a void method you don't need the else condition
+        //if (!isFull()) data[++topIndexLocation] = value;
+        // if you prefer one line. Since this is a void method you don't need the else condition
     }
     public void pop() {
         if (!isEmpty()) {
-            topIndexLocation--; // This is equivalent to topIndexLocation = (topIndexLocation - 1);
+            topIndexLocation--;
+            // This is equivalent to topIndexLocation = (topIndexLocation - 1);
         } else {
             throw new IndexOutOfBoundsException("Stack is empty");
         }
     }
     //second version, you can use pop to return data too.
-    //public T pop() {
+    //public int pop() {
     //  if (!isEmpty()) {
-    //      T valueToBeRemove = data[topIndexLocation];
-    //      data[topIndexLocation] = null;
+    //      int valueToBeRemove = data[topIndexLocation];
+    //      data[topIndexLocation] = 0; // int not is not object.
     //      topIndexLocation--;
     //      return valueToBeRemove;
     //  } else {
-    //      return null;
+    //      throw new IndexOutOfBoundsException("Stack is empty");
     //  }
     //}
     public int top() {
@@ -69,9 +64,11 @@ public class IntStack {
         return data[topIndexLocation];
     }
     public int size() {
-        return topIndexLocation+1; // Since the topIndexLocation is set to -1,
-        // when it is incremented such as -1+1 = 0, the first value will go to index zero.
-        //Thus, topIndexLocation match the indices of the array, but we the size is required,
+        return topIndexLocation+1;
+        // Since the topIndexLocation is set to -1, and it increments with the index numbers,
+        // in order to get data.lengthy, we will add 1. Ex. The last array index location is
+        // data.length-1, but to get the data.length.
+        //Thus, topIndexLocation match the indices of the array, but when the size is required,
         // then topIndexLocation +1 will give a size which counts from 1 up to the inserted elements.
     }
 }
--- Main.java
+++ Main.java
@@ -1,38 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-//        Stack<Integer> intStack = new Stack<>(3);
-//        intStack.push(199);
-//        intStack.push(200);
-//
-//        System.out.println(intStack.size());
-//
-//        System.out.println(intStack.top());
-//        intStack.pop();
-//        System.out.println(intStack.top());
-//        intStack.pop();
-        
-        
-//        intStack.push(200);
-//        intStack.pop();
-//        intStack.pop();
-//        intStack.pop();
-//        intStack.push(500);
-//        System.out.println(intStack.isEmpty());
-//        System.out.println(intStack.isFull());
-//        System.out.println(intStack.top());
-//        System.out.println(intStack.isFull() + ", isEmpty " + intStack.isEmpty());
-//        System.out.println(intStack.top());
-//        intStack.pop();
-//        intStack.pop();
-//        intStack.pop();
-//        System.out.println(intStack.isEmpty());
+        Stack<Integer> intStack = new Stack<>(3);
+        intStack.push(199);
+        intStack.push(200);
 
-//        Stack<String> stringStack = new Stack<>(4);
-//        stringStack.push("Hello world");
-//        System.out.println(stringStack.size());
-////        stringStack.pop();
-//        System.out.println(stringStack.size());
-//        System.out.println(stringStack.isEmpty());
+        System.out.println(intStack.size());
+
+        System.out.println(intStack.top());
+        intStack.pop();
+        System.out.println(intStack.top());
+        intStack.pop();
+        
+        
+        intStack.push(200);
+        intStack.pop();
+        intStack.pop();
+        intStack.pop();
+        intStack.push(500);
+        System.out.println(intStack.isEmpty());
+        System.out.println(intStack.isFull());
+        System.out.println(intStack.top());
+        System.out.println(intStack.isFull() + ", isEmpty " + intStack.isEmpty());
+        System.out.println(intStack.top());
+        intStack.pop();
+        intStack.pop();
+        intStack.pop();
+        System.out.println(intStack.isEmpty());
+
+        Stack<String> stringStack = new Stack<>(4);
+        stringStack.push("Hello world");
+        System.out.println(stringStack.size());
+//        stringStack.pop();
+        System.out.println(stringStack.size());
+        System.out.println(stringStack.isEmpty());
         StringStack st = new StringStack(1);
         st.push("Hello world");
         System.out.println(st.isFull());
@@ -40,6 +40,16 @@ public class Main {
         System.out.println(st.isEmpty());
         System.out.println(st.top());
         st.pop();
+
+
+        Stack<Integer> numberStack = new Stack<>(3);
+        Stack<String> stringStack1 = new Stack<>(6);
+        Stack<Double> doubleStack = new Stack<>(100);
+        //By using a generic stack, we eliminated the need for IntStack and StringStack class.
+
+
+
+
 
 
     }
